7c9e8ba65f3100ba5cd4d4990e21934e1e8d53e1
james gill <jimmygill28@hotmail.com>
2020-09-15 17:24:13 +0100

    fix it josh

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import Profile from "./screens/Profile";
 
-export default function App() {
+export default function App(props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Login nav={props.navigation} />
+      {/* <Login /> */}
+      {/* <Profile /> */}
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config/Firebase.js
@@ -0,0 +1,33 @@
+import firebase from "firebase";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  MESSAGE_SENDER_ID,
+  APP_ID,
+} from "react-native-dotenv";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfyvOon68vUfa5qMZoMhRt-yuPZxwLqr8",
+  authDomain: "my-awesome-project-42e39.firebaseapp.com",
+  databaseURL: "https://my-awesome-project-42e39.firebaseio.com",
+  projectId: "my-awesome-project-42e39",
+  storageBucket: "my-awesome-project-42e39.appspot.com",
+  messagingSenderId: "1046433424488",
+  appId: "1:1046433424488:web:54eaea0514a08a09e947dd",
+};
+// const firebaseConfig = {
+//   apiKey: API_KEY,
+//   authDomain: AUTH_DOMAIN,
+//   databaseURL: DATABASE_URL,
+//   projectId: PROJECT_ID,
+//   storageBucket: "",
+//   messagingSenderId: MESSAGE_SENDER_ID,
+//   appId: APP_ID,
+// };
+
+// Initialize Firebase
+const Firebase = firebase.initializeApp(firebaseConfig);
+
+export default Firebase;
